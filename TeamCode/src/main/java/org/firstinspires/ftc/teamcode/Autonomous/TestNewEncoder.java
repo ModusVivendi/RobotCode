6e3ef1e4d8a77eb0d51137f8026eff48adf77890
DavidInfo16 <simoc.david@eminescu.rdsor.ro>
2022-12-29 17:26:08 +0200

    update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/TestNewEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/TestNewEncoder.java
@@ -6,32 +6,38 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.teamcode.Functions.ArmEncoder;
 import org.firstinspires.ftc.teamcode.Functions.NewEncoderMove;
 
 @Autonomous(name="EncoderNew", group = "Concept")
 //@Disabled
 public class TestNewEncoder extends LinearOpMode {
 
-    private DcMotor leftMotor, leftMotorBack, rightMotor, rightMotorBack;
+   // private DcMotor leftMotor, leftMotorBack, rightMotor, rightMotorBack;
+    private DcMotor armMotor;
     private NewEncoderMove encoderMove;
-
+    private ArmEncoder armEncoder;
 
     @Override
     public void runOpMode()
     {
-        leftMotor = hardwareMap.dcMotor.get("FL");
-        leftMotorBack = hardwareMap.dcMotor.get("BL");
-        rightMotor = hardwareMap.dcMotor.get("FR");
-        rightMotorBack = hardwareMap.dcMotor.get("BR");
+      //  leftMotor = hardwareMap.dcMotor.get("FL");
+       // leftMotorBack = hardwareMap.dcMotor.get("BL");
+       // rightMotor = hardwareMap.dcMotor.get("FR");
+       // rightMotorBack = hardwareMap.dcMotor.get("BR");
+        armMotor = hardwareMap.dcMotor.get("AM");
+       // encoderMove = new NewEncoderMove(leftMotor,leftMotorBack,rightMotor,rightMotorBack);
 
-        encoderMove = new NewEncoderMove(leftMotor,leftMotorBack,rightMotor,rightMotorBack);
+        armEncoder = new ArmEncoder(armMotor);
 
         waitForStart();
 
-        encoderMove.DriveTo(1000,1000,1000,1000,0.5, opModeIsActive());
+        armEncoder.goTo(1540, 0.5);
         sleep(1000);
-        encoderMove.DriveTo(-1000,1000,1000,-1000,0.5,opModeIsActive());
-        sleep(500);
+       // encoderMove.DriveTo(1000,1000,1000,1000,0.5, opModeIsActive());
+      //  sleep(1000);
+      //  encoderMove.DriveTo(-1000,1000,1000,-1000,0.5,opModeIsActive());
+      //  sleep(500);
     }
 }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Functions/ArmEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Functions/ArmEncoder.java
@@ -17,6 +17,7 @@ public class ArmEncoder {
          * Runs the motor using encoders.
          */
         armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         armPos = 0;
     }
     public ArmEncoder(DcMotor _AM)
@@ -27,15 +28,24 @@ public class ArmEncoder {
 
     public void goTo(int armTarget, double power)
     {
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
         armPos = 0;
+
         armPos+=armTarget;
+
         armMotor.setTargetPosition(armPos);
+
         armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
         armMotor.setPower(power);
 
         while(armMotor.isBusy())
         {
 
         }
+
+       // armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
     }
 }
