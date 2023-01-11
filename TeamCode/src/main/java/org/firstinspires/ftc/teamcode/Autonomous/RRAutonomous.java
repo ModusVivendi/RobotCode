142bcd4c1ae411991006eb72310a9736dbdaf016
DavidInfo16 <simoc.david@eminescu.rdsor.ro>
2023-01-11 19:12:31 +0200

    Create RRAutonomous.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RRAutonomous.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.RoadRunner.drive.SampleMecanumDrive;
+
+@Autonomous
+public class RRAutonomous extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        Trajectory trajectory1 = drive.trajectoryBuilder(new Pose2d())
+                .forward(10)
+                .build();
+        waitForStart();
+        if(isStopRequested()) return;
+        drive.followTrajectory(trajectory1);
+    }
+}
