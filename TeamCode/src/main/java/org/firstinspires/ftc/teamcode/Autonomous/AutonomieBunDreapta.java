83b68dfe41a2cef033e779bc88b2b75a97898a3a
Denisa Derședan <dersedand@gmail.com>
2023-01-30 16:46:31 +0200

    disabled all

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomieBunDreapta.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomieBunDreapta.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -13,6 +14,7 @@ import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 
 @Autonomous(name = "AutonomieBun Dreapta")
+@Disabled
 public class AutonomieBunDreapta extends LinearOpMode {
 
     private SleeveDetection sleeveDetection;
